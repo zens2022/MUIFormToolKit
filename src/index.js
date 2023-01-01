ad2b9ca209bfe396ea05065a9eadefa97cc078a8
kamicreator <kamicreator@gmail.com>
2023-01-01 22:41:25 +0800

    [add] build react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <>Base</>
+  </React.StrictMode>
+);
+
+reportWebVitals();
